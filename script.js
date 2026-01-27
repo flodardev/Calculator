665f057a74c6eaa01176ed7a684f8b20e6523113
flodardev <63950579+flodardev@users.noreply.github.com>
2026-01-27 09:37:01 +0800

    Add basic math functions in script.js
    Add
    Substract
    Multiply
    Divide

---
+++ script.js
@@ -0,0 +1,18 @@
+// Basic math operators
+console.log(add(1, 2))
+
+function add(a, b) {
+    return a + b;
+}
+
+function substract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
